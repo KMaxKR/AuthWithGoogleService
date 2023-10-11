f826668693fa285ebfb4d88ec5a2df041bd21354
KiRa <cresciucmax@gmail.com>
2023-10-11 17:40:03 +0300

    Initial commit

---
+++ AuthWithGoogleService/src/main/java/ks/msx/AuthWithGoogleService/AuthWithGoogleServiceApplication.java
@@ -0,0 +1,13 @@
+package ks.msx.AuthWithGoogleService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthWithGoogleServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthWithGoogleServiceApplication.class, args);
+	}
+
+}
